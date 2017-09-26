614194e66892a4be6e9f92e8939364c1e634dc26
marieantoinetteontopoftheworld <jack.heseltine@gmail.com>
2017-09-26 13:59:14 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,9 +8,6 @@ end
 
 # code your input_to_index and move method here!
 def move(board, index, character = 'X')
-
-  board[index - 1] = character
-
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
