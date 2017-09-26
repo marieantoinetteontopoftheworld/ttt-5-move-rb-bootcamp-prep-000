efac272c26c65557b6df9f558d0afd4eeebcf8d3
marieantoinetteontopoftheworld <jack.heseltine@gmail.com>
2017-09-26 14:00:23 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,6 +7,10 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input)
+  return input - 1
+end
+ 
 def move(board, index, character = 'X')
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
