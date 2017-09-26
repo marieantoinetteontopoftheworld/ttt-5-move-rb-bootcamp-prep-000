49182b7a02fc91764784945f4c4a1d40c4f957a0
marieantoinetteontopoftheworld <jack.heseltine@gmail.com>
2017-09-26 13:44:38 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,7 @@
-def display_board(board)
+def display_board(board, index, character = 'X')
+  
+  board[index] = character
+  
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
